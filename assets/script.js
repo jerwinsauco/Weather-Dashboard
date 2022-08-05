b7d516e3e764b566f59ffb0c6d2f16cbb4d99298
Jerwin <jerwinsauco@gmail.com>
2022-08-05 17:38:43 -0400

    first commit

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+// PseudoCode Weather Dashboard
+// Create variables for the weather API
+// Create a function to get the weather of the selected City
+// Selected City is displayed in the header card
+// Seleted City is stored in local storage
+
+// Create a function to get the date
+// Create a function to get the time
+// Create a function to get the location
+// Create a function to get the weather icon
+// Create a function to get the weather description
+// Create a function to get the temperature
+// Create a function to get the humidity
+// Create a function to get the wind speed
+// Create a function to get the wind direction
+// Create a function to get the visibility
+// Create a function to get the sunrise
+// Create a function to get the sunset
+// Create a function to get the UV index
+// Create a function whether uv index is low, moderate, high, or extreme
